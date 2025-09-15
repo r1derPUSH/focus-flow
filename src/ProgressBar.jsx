da286bbec717fb9256af671d7abf09a569bf5917
Maksym Kopak <maksymkopak@MacBook-Air-Maksym.local>
2025-09-15 22:28:27 +0300

    Created progress-bar item

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -3,6 +3,17 @@ import { useState } from "react";
 function ProgressBar () {
     const [tasks, setTasks] = useState([]);
 
+    return (
+        <section>
+            <div className="progress-bar">
+                <div className="progress-bar-outside"> 
+                    <div className="progress-bar-inside">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default ProgressBar;
