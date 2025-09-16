4179756d91342fefcdd9c933218bb0332faf9ef9
r1derPUSH <riderworkable1234@gmail.com>
2025-09-16 16:35:24 +0300

    Added some styles for clock

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function DigitalClock () {
-
+function DigitalClock() {
+  return (
+    <>
+      <div className="clock-container">
+        <div className="clock">
+          <span>00:00:00</span>
+        </div>
+      </div>
+    </>
+  );
 }
 
- export default DigitalClock;
+export default DigitalClock;
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import ProgressBar from "./ProgressBar";
+import DigitalClock from "./DigitalClock";
 
 const MainContent = ({ name, surname }) => {
-
-  
-
   return (
     <section className="body-section">
       <Header name={name} surname={surname} />
       <ProgressBar />
+      <DigitalClock />
     </section>
   );
 };
