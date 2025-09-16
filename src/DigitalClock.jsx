b4506a91329093b765ee0033bc5d685c34d2acb9
r1derPUSH <riderworkable1234@gmail.com>
2025-09-16 16:56:39 +0300

    finished digital clock js functionality

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,6 +1,30 @@
 import { useState, useEffect } from "react";
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function formatTime() {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
+  }
+
+  function padZero(number) {
+    return (number < 10 ? "0" : "") + number;
+  }
+
   return (
     <>
       <div className="clock-container">
