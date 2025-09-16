d4e7fd6f0e343b7de90152ed67cdc042459371b4
r1derPUSH <riderworkable1234@gmail.com>
2025-09-16 21:24:19 +0300

    Created handleAddTask fn & div for uidesc

--- src/DailyTasks.jsx
+++ src/DailyTasks.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 function DailyTasks() {
-  function handleAddTask() {
-    console.log("worked out");
-  }
+  const [tasks, setTasks] = useState([]);
+  const [isUserDescriptionVisible, setIsUserDescriptionVisible] =
+    useState(false);
+  function handleAddTask() {}
   return (
     <>
       <div className="tasks-flex-div">
         <div className="tasks-div">
           <span className="span-daily-tasks-text">Daily Tasks: </span>
+          <div className="task-user-description">
+            {isUserDescriptionVisible ? "Bro" : ""}
+          </div>
           <button onClick={handleAddTask} className="add-task-btn">
             Add New Task
           </button>
