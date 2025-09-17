d2fdfb978ae4b444bc29ba5cf92bf610a8155ba4
r1derPUSH <riderworkable1234@gmail.com>
2025-09-17 20:51:13 +0300

    created states for useState in udesc component

--- src/UserDescriptionOfTask.jsx
+++ src/UserDescriptionOfTask.jsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
 
 function UserDescriptionOfTask() {
+  const [taskValue, setTaskValue] = useState("");
   const [task, setTask] = useState("");
+  const [descValue, setDescValue] = useState("");
   const [desc, setDesc] = useState("");
-  const [difficulty, setDifficulty] = useState("");
+  // const [difficulty, setDifficulty] = useState("");
+
+  const handleCreate = () => {
+    setTask(taskValue);
+    setDesc(descValue);
+  };
+
+  const handleChangeTask = (e) => {
+    setTaskValue(e.target.value);
+  };
+
+  const handleChangeDesc = (e) => {
+    setDescValue(e.target.value);
+  };
+
   return (
     <div className="user-description-of-task-container">
-      <input className="input-uDesc" type="text" placeholder="Task" />
-      <input className="input-uDesc" type="text" placeholder="Description" />
+      <input
+        className="input-uDesc"
+        value={task}
+        type="text"
+        onChange={handleChangeTask}
+        placeholder="Task"
+      />
+      <input
+        className="input-uDesc"
+        value={desc}
+        type="text"
+        onChange={handleChangeDesc}
+        placeholder="Description"
+      />
       <div className="dropbox-bar">
-        <button className="create-task-button">Create</button>
+        <button className="create-task-button" onClick={handleCreate}>
+          Create
+        </button>
       </div>
     </div>
   );
