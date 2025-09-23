b75f828bbb210819ec5a59935f32b47acd0a9c9d
r1derPUSH <riderworkable1234@gmail.com>
2025-09-23 20:07:54 +0300

    Created new componen UserTask

---
+++ src/UserTask.jsx
@@ -0,0 +1,13 @@
+function UserTask() {
+  return (
+    <div className="user-task">
+      <span className="task-name">Task</span>
+      <span className="task-description">Desc</span>
+      <div className="task-buttons">
+        <button className="focus">Focus</button>
+      </div>
+    </div>
+  );
+}
+
+export default UserTask;
