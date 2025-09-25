310b89b0800967ce6cf18c022f13c66b89cdecd2
r1derPUSH <riderworkable1234@gmail.com>
2025-09-25 22:43:34 +0300

    Optimising progress bar

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
-function ProgressBar () {
-    const [tasks, setTasks] = useState([]);
+function ProgressBar() {
+  const [tasks, setTasks] = useState([]);
 
-    return (
-        <section className="progress-bar-section">
-            <div className="progress-bar">
-                <div className="progress-bar-outside"> 
-                    <div className="progress-bar-inside">
-                        
-                    </div>
-                </div>
+  return (
+    <section className="progress-bar-section">
+      <div className="progress-bar">
+        <div className="progress-bar-box">
+          <div className="text-and-bar">
+            <span className="progressBar-text">Your Progress...</span>
+            <div className="progress-bar-outside">
+              <div className="progress-bar-inside"></div>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ProgressBar;
