ce3ee5a246c87220d8206a81ce03746953eb4281
r1derPUSH <riderworkable1234@gmail.com>
2025-09-27 17:14:18 +0300

    Adding styles for focusMode

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -30,15 +30,27 @@ function ProgressBar({ totalTasks, completedTasks, isFocusMode }) {
           : "progress-bar-section-focusModeActivated"
       }
     >
-      <div className="focusMode-task-container">
-        <span className="focusMode-task-name"></span>
-        <span className="focusMode-task-description"></span>
-      </div>
-      <div className="focusMode-pomodoro-timer">
-        <div className="focusMode-pomodoro-btns">
-          <button>Start</button>
-          <input type="text" placeholder="Set work time in minutes" />
-          <input type="text" placeholder="Set break time in minutes" />
+      <div
+        className={!isFocusMode ? "focusMode-box-disabled" : "focusMode-box"}
+      >
+        <div className="focusMode-task-container">
+          <span className="focusMode-task-name">Task 123</span>
+          <span className="focusMode-task-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </span>
+        </div>
+        <div className="focusMode-pomodoro-timer">
+          <div className="focusMode-pomodoro-btns">
+            <button>Start</button>
+            <input type="text" placeholder="Set work time in minutes" />
+            <input type="text" placeholder="Set break time in minutes" />
+          </div>
+        </div>
+        <div className="focusMode-tips">
+          <span>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut ea
+            placeat, laudantium dolor omnis similique.
+          </span>
         </div>
       </div>
     </section>
