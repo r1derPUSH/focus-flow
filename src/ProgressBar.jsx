5fe5a230d7bf935893485769e1c643a174c6200a
r1derPUSH <riderworkable1234@gmail.com>
2025-09-27 17:08:54 +0300

    Creating pomodoto/timer

--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -15,6 +15,11 @@ function ProgressBar({ totalTasks, completedTasks, isFocusMode }) {
   const [tasks, setTasks] = useState([]);
   const [progressBarValue, setProgressBarValue] = useState(avg);
 
+  //
+
+  const [workTime, setWorkTime] = useState(0);
+  const [breakTime, setBreakTime] = useState(0);
+
   //   const completedTasks =
 
   return (
@@ -28,6 +33,13 @@ function ProgressBar({ totalTasks, completedTasks, isFocusMode }) {
       <div className="focusMode-task-container">
         <span className="focusMode-task-name"></span>
         <span className="focusMode-task-description"></span>
+      </div>
+      <div className="focusMode-pomodoro-timer">
+        <div className="focusMode-pomodoro-btns">
+          <button>Start</button>
+          <input type="text" placeholder="Set work time in minutes" />
+          <input type="text" placeholder="Set break time in minutes" />
+        </div>
       </div>
     </section>
   );
