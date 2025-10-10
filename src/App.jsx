2d46d6d43cc1baf79b0c2881216161133fd9eebb
r1derPUSH <riderworkable1234@gmail.com>
2025-10-10 21:35:16 +0300

    Changed styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Register from "./Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./Register";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CompletedTasks from "./CompletedTasks";
 
 function App() {
   return (
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Register />}></Route>
+    //     <Route path="/" element={<CompletedTasks />}></Route>
+    //   </Routes>
+    // </Router>
     <>
       <Register />
     </>
