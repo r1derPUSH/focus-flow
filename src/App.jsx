0ed00a41aba9d8d258dc11c36e18128ce8b344cf
r1derPUSH <riderworkable1234@gmail.com>
2025-10-10 21:31:21 +0300

    Created new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Register from "./Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./Register";
 
 function App() {
   return (
     <>
-      <Register />      
+      <Register />
     </>
   );
 }
---
+++ src/CompletedTasks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CompletedTasks() {
+  return <div></div>;
+}
+
+export default CompletedTasks;
